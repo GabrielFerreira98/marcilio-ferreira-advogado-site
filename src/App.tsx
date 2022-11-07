5dd28a13cd65893b65aabb53fbdaef5ac8b1abd1
Gabriel Ferreira <gdafgabriel@gmail.com>
2022-11-07 18:32:12 -0300

    Adição do botão do wpp e ajuste de imagem de notícia

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { About } from "./Pages/About"
 import { Contact } from "./Pages/Contact"
 import { Notices } from "./Pages/Notices"
 
+import { WhatsappLogo } from 'phosphor-react'
+
 function App() {
   return (
     <>
@@ -20,6 +22,12 @@ function App() {
       <Contact/>
 
       <Footer/>
+
+      <div className="h-12 w-12 bg-emerald-400 rounded-full fixed bottom-4 right-4 flex items-center justify-center cursor-pointer hover:bg-emerald-500 transition 300ms">
+        <WhatsappLogo color="white" size={32}/>
+      </div>
+      
+      
     </>
   )
 }
--- src/Components/NoticeContainer.tsx
+++ src/Components/NoticeContainer.tsx
@@ -7,7 +7,7 @@ interface NoticeContainer{
 export function NoticeContainer(NoticeProps: NoticeContainer){
     return(
         <div className="bg-white h-2/3 w-96 rounded-xl flex flex-col items-center cursor-pointer hover:translate-x-[-30px] hover:translate-y-[-30px] transition 300ms">
-            <img className="w-[80%] mt-8 rounded-2xl shadow-xl" src={NoticeProps.url} alt="Notícia" />
+            <img className="w-[80%] h-[40%] mt-8 rounded-2xl shadow-xl" src={NoticeProps.url} alt="Notícia" />
             <div className="w-[80%] mt-8 text-left text-marcilio-blue-700 font-semibold text-2xl">{NoticeProps.title}</div>
             <div className="w-[80%] mt-4 text-justify text-marcilio-blue-500 font-semibold text-md">{NoticeProps.subtitle}</div>
         </div>
