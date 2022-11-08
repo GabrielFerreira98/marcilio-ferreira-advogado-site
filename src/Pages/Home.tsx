c511352030b6166c99997e10c7c2bd2cabe5894e
WeslleyAnd <jose_weslley_31@hotmail.com>
2022-11-07 23:07:24 -0300

    ajustes no style

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,26 +3,18 @@ import { ContainerArea } from "../Components/ContainerArea";
 
 export function Home(){
   return(
-    <div className="h-[88vh] bg-marcilio-blue-300 flex flex-col items-center justify-center">
-
-    <div className="flex items-center justify-center gap-32">
-      <ContainerArea children="Administrativo" ></ContainerArea>
-      <ContainerArea children="Penal" ></ContainerArea>
-      <ContainerArea children="Civil" ></ContainerArea>
-    </div>
-
-    <div className="flex items-center justify-center gap-32 mt-12">
-      <ContainerArea children="Administrativo" ></ContainerArea>
-      <ContainerArea children="Penal" ></ContainerArea>
-      <ContainerArea children="Civil" ></ContainerArea>
-    </div>
-
-    <div className="flex items-center justify-center gap-32 mt-12">
-      <ContainerArea children="Administrativo" ></ContainerArea>
-      <ContainerArea children="Penal" ></ContainerArea>
-      <ContainerArea children="Civil" ></ContainerArea>
-    </div>
-
+    <div className="h-[88vh] bg-marcilio-blue-300 flex items-center justify-center">
+      <section className="h-[50%] w-[80%] bg-yellow-500 flex flex-wrap items-center justify-between">
+          <ContainerArea children="Administrativo" ></ContainerArea>
+          <ContainerArea children="Penal" ></ContainerArea>
+          <ContainerArea children="Civil" ></ContainerArea>
+          <ContainerArea children="Administrativo" ></ContainerArea>
+          <ContainerArea children="Penal" ></ContainerArea>
+          <ContainerArea children="Civil" ></ContainerArea>
+          <ContainerArea children="Administrativo" ></ContainerArea>
+          <ContainerArea children="Penal" ></ContainerArea>
+          <ContainerArea children="Civil" ></ContainerArea>
+      </section>
     </div>
   )   
 }
